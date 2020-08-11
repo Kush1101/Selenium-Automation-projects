e58de334ca03abbaccd354f83f31ebe3d457bf9c
Kushagra Bansal <kushagrabansalajmer@gmail.com>
2020-08-11 14:11:42 +0530

    Add files via upload

--- Swayam Login Automation/swayam automation.py
+++ Swayam Login Automation/swayam automation.py
@@ -10,37 +10,51 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
-
 from selenium.webdriver.common.by import By
 import time
 driver = webdriver.Chrome()
-
+#get the site
 driver.get("https://swayam.gov.in/")
 wait = WebDriverWait(driver,600)
+
 driver.find_element_by_name("submit").click()
-x_arg = """//*[@id="GoogleExchange"]"""
-target = wait.until(EC.presence_of_element_located((By.XPATH,x_arg)))
-target.click()
-arg2 = '//*[@id="identifierId"]'
-#username = driver.find_element_by_class_name('')
-username = driver.find_element_by_xpath(arg2)
-email = 'your email'
-username.send_keys(email+Keys.ENTER)
-#driver.find_element_by_class_name('VfPpkd-RLmnJb').click()
 
-#wait2 = WebDriverWait(driver,1000)
-x_arg2 = '//*[@id="password"]/div[1]/div/div[1]/input'
+#Google login button
+google_xpath = '''//*[@id="GoogleExchange"]'''
+google = wait.until(EC.presence_of_element_located((By.XPATH,google_xpath)))
+google.click()
 
-class_name = 'whsOnd zHQkBf'
-#password = driver.find_element_by_class_name(class_name)
-password = driver.find_element_by_xpath(x_arg2)
-#assword = driver.find_element_by_id("user_password")
-#password = wait2.until(EC.presence_of_element_located((By.XPATH,'//*[@id="password"]/div[1]/div/div[1]/input')))
-#password.click()
-#wait2.until(EC.elementToBeClickable(password))
-#password.click()
-pwd = 'Hidden'
-password.send_keys(pwd+Keys.ENTER)
+#Username on google login
+username_xpath = '//*[@id="identifierId"]'
+username = driver.find_element_by_xpath(username_xpath)
+email = 'kushagra.nptel@gmail.com'
+username.send_keys(email)
+#next button
+nextButton = driver.find_elements_by_xpath('//*[@id ="identifierNext"]') 
+nextButton[0].click() 
+
+pwd = '2_Akrs@26174'
+password_xpath = '//*[@id ="password"]/div[1]/div / div[1]/input'
+passWordBox = wait.until(EC.element_to_be_clickable((By.XPATH,password_xpath)))
+passWordBox.send_keys(pwd)
+#next button
+nextButton = driver.find_elements_by_xpath('//*[@id ="passwordNext"]') 
+nextButton[0].click() 
+#BINGO!
+
 print("Successfully logged in!")
- 
-#""" //*[@id="password"]/div[1]/div/div[1]/input //*[@id="password"]/div[1]/div/div[1]/input
+#wait until loading finishes
+wait2 = WebDriverWait(driver,600)
+
+#dropdown 
+dropdown_path = '//*[@id="bs-example-navbar-collapse-1"]/ul/li[3]/a'
+dropdown = wait2.until(EC.presence_of_element_located((By.XPATH,dropdown_path)))
+dropdown.click() 
+
+#my courses tab
+mycourse_path = '//*[@id="bs-example-navbar-collapse-1"]/ul/li[3]/ul/li[2]/a' 
+mycourse = wait2.until(EC.presence_of_element_located((By.XPATH,mycourse_path)))
+mycourse.click()
+
+#Did it!
+print("Your courses tab is now open! Thank You!!")
